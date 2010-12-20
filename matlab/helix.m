2e64a4db02705741d631bde29d243f4f642a3fd4
Gergely Imreh <imrehg@gmail.com>
2010-12-20 19:18:35 +0800

    starting to modify matlab code

--- matlab/helix.m
+++ matlab/helix.m
@@ -1,16 +1,11 @@
 function Bz = helix(c,R,z)
-    c1 = c(1);
-    c2 = c(2);
-    c3 = c(3);
-    c4 = c(4);
-    c5 = c(5);
-   c6 = c(6);
-   c7 = c(7);
-   c8 = c(8);
+    c1d = polyder(c(1:7));
+    c2 = c(8:9);
 
-    for i=1:length(z);
-      intc = @(p) (c3+2.*c4.*p+3*c5.*p.^2+c6.*p.^3+c7.*p.^4+c8.*p.^5)./sqrt(R^2+(c1+c2.*p-z(i)).^2).^3;
-       Temp(i) = quad(intc, 0, 2*pi);
+    Temp = zeros(length(z),1);
+    for i = 1:length(z)
+        intc = @(p) polyval(c1d, p) / sqrt(R^2 + (polyval(c2, 0)-z(i))^2)^3;
+        field(i) = quad(intc, 0, 2*pi);
     end
-    Bz = R.^2.*Temp;
+    Bz = R^2 * field;
 end
